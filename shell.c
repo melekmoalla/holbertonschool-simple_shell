ec1d12cb4e087d61bd02d298cdaef9dfdad7ee67
melekmoalla <melekmoalla2468@gmail.com>
2022-12-01 01:39:34 +0100

    test

--- shell.c
+++ shell.c
@@ -12,14 +12,14 @@ int main(__attribute__((unused)) int argc, __attribute__((unused)) char **argv)
     int wa9if;
     while (1)
     {
-        printf("mayouka>>");
+        printf(">");
         line = read_line();
         args = paress(line);
         wa9if = execute(args);
-        if (wa9if == 0)
-            break;
         free(line);
         free(args);
+        if (wa9if == 0)
+            break;
     }
     return (EXIT_SUCCESS);
 }
