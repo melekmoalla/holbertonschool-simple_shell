6c57e632de0709d8be543037bf0b9793020b51c3
melekmoalla <melekmoalla2468@gmail.com>
2022-12-01 11:17:22 +0100

    test

--- read.c
+++ read.c
@@ -1,43 +1,23 @@
 #include "simple.h"
 
 char *read_line(void)
+
 {
-    int size = 1024, pos = 0, c;
-    char *buf = malloc(sizeof(char) * size);
+    char *line = NULL;
+    size_t bufsize = 0;
 
-    if (!buf)
+    if (getline(&line, &bufsize, stdin) == -1)
     {
-        fprintf(stderr, "shell: allocation error\n");
-        exit(EXIT_FAILURE);
-    }
-    while (1)
-    {
-        c = getchar();
-
-        if (c == EOF)
+        if (feof(stdin))
         {
             exit(EXIT_SUCCESS);
         }
-        else if (c == '\n')
-        {
-            buf[pos] = '\0';
-            return (buf);
-        }
         else
         {
-            buf[pos] = c;
-        }
-        pos++;
-
-        if (pos >= size)
-        {
-            size += 1024;
-            buf = realloc(buf, size);
-            if (!buf)
-            {
-                fprintf(stderr, "lsh: allocation error\n");
-                exit(EXIT_FAILURE);
-            }
+            perror("readline");
+            exit(EXIT_FAILURE);
         }
     }
+
+    return (line);
 }
