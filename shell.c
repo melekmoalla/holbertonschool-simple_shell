b0c0506ee6e3804e3d8f16efec4f4a65ff31af1a
melekmoalla <melekmoalla2468@gmail.com>
2022-12-06 10:11:51 +0100

    test if else get line

--- shell.c
+++ shell.c
@@ -1,54 +1,69 @@
 #include "simple.h"
-#include <unistd.h>
 /**
  * main - master fonction
  * Return: 0
- */
+ **/
 int main(void)
 {
-	int a, parsedStrLen;
+	int status, parsedStrLen;
 	pid_t id;
 	size_t size = 32;
-	char buffer[100], *sentence = buffer, **parsedStr;
+	char buffer[32], *sentence = buffer, **parsedStr;
+
 	while (1)
 	{
-		if (getline(&sentence, &size, stdin) != -1)
+
+		while (1)
 		{
-			parsedStrLen = numOfWords(sentence);
-			if (parsedStrLen > 0)
+			if (getline(&sentence, &size, stdin) == -1)
 			{
-				parsedStr = (char **)malloc((parsedStrLen + 1) * sizeof(char *));
-				if (parsedStr == NULL)
-				{
-					fprintf(stderr, "malloc failed");
-					exit(1);
-				}
-				parsedStr[parsedStrLen] = NULL;
-				parseString(sentence, parsedStr);
-				if (strcmp(parsedStr[0], "exit") == 0)
-				{
-					free(parsedStr);
-					exit(0);
-				}
-				id = fork();
-				if (id < 0)
-				{
-					perror("");
-					freeArr(parsedStr);
-					exit(1);
-				}
-				if (id == 0)
-				{
-					a = exeCommand(parsedStr);
-					if (a == 127)
-					{
-						return (127);
-					}
-				}
+				if (feof(stdin))
+					exit(EXIT_SUCCESS);
 				else
 				{
 					wait(NULL);
-					freeArr(parsedStr);
+					return (0);
+				}
+			}
+			else
+			{
+				parsedStrLen = numOfWords(sentence);
+				if (parsedStrLen > 0)
+				{
+					parsedStr = (char **)malloc((parsedStrLen + 1) * sizeof(char *));
+					if (parsedStr == NULL)
+					{
+						fprintf(stderr, "malloc failed");
+						return (1);
+					}
+					parsedStr[parsedStrLen] = NULL;
+					parseString(sentence, parsedStr);
+					id = fork();
+					if (strcmp(parsedStr[0], "exit") == 0)
+					{
+						freeArr(parsedStr);
+						return (0);
+					}
+					else if (id < 0)
+					{
+						perror("ERR");
+						freeArr(parsedStr);
+						exit(1);
+					}
+					else if (id == 0)
+					{
+						a = exeCommand(parsedStr);
+						freeArr(parsedStr);
+						if (a == 1)
+						{
+							return (127);
+						}
+					}
+					else
+					{
+						wait(NULL);
+						freeArr(parsedStr);
+					}
 				}
 			}
 		}
