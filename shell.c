7d45f599b7077b9f92246cfa613811ac25ffb337
melekmoalla <melekmoalla2468@gmail.com>
2022-12-06 15:42:05 +0100

    retuen the first code

--- shell.c
+++ shell.c
@@ -3,49 +3,65 @@
  * main - master fonction
  * Return: 0
  **/
-int main()
+int main(void)
 {
+	int parsedStrLen;
 	pid_t id;
-	char sentence[SENTENCE_LEN];
-	char **parsedStr;
-	int numOfCommands = 0, lengthOfCommands = 0, parsedStrLen;
+	size_t size = 1024;
+	char buffer[1024], *sentence = buffer, **parsedStr;
 
 	while (1)
 	{
-		if (fgets(sentence, SENTENCE_LEN, stdin) != NULL)
+
+		while (1)
 		{
-			parsedStrLen = numOfWords(sentence);
-			lengthOfCommands += (int)(strlen(sentence) - 1);
-			numOfCommands++;
-			if (parsedStrLen > 0)
+			if (getline(&sentence, &size, stdin) == -1)
 			{
-				parsedStr = (char **)malloc((parsedStrLen + 1) * sizeof(char *));
-				if (parsedStr == NULL)
-				{
-					fprintf(stderr, "malloc failed");
-					exit(1);
-				}
-				parsedStr[parsedStrLen] = NULL;
-				parseString(sentence, parsedStr);
-				id = fork();
-				if (id < 0)
-				{
-					perror("ERR");
-					freeArr(parsedStr);
-					exit(1);
-				}
-				else if (id == 0)
-				{
-					exeCommand(parsedStr);
-					freeArr(parsedStr);
-				}
+				if (feof(stdin))
+					return (EXIT_SUCCESS);
 				else
 				{
-					wait(NULL);
+					free(sentence);
+					return (0);
+				}
+			}
+			else
+			{
+				parsedStrLen = numOfWords(sentence);
+				if (parsedStrLen > 0)
+				{
+					parsedStr = (char **)malloc((parsedStrLen + 1) * sizeof(char *));
+					if (parsedStr == NULL)
+					{
+						fprintf(stderr, "malloc failed");
+						return (1);
+					}
+					parsedStr[parsedStrLen] = NULL;
+					parseString(sentence, parsedStr);
+					id = fork();
+					if (strcmp(parsedStr[0], "exit") == 0)
+					{
+						freeArr(parsedStr);
+						return (0);
+					}
+					else if (id < 0)
+					{
+						perror("ERR");
+						freeArr(parsedStr);
+						exit(1);
+					}
+					else if (id == 0)
+					{
+						exeCommand(parsedStr);
+					}
+					else
+					{
+						wait(NULL);
+					}
 					freeArr(parsedStr);
 				}
 			}
 		}
 	}
-	return 0;
+	exit(0);
 }
