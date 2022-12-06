6abeae00e48c7b080cd7d5f1f262c7e477e72f3a
melekmoalla <melekmoalla2468@gmail.com>
2022-12-06 18:37:51 +0100

    swith else down

--- shell.c
+++ shell.c
@@ -11,45 +11,48 @@ int main(void)
 	char buffer[1024], *sentence = buffer, **parsedStr;
 	while (1)
 	{
-		if (getline(&sentence, &size, stdin) != -1)
-			parsedStrLen = numOfWords(sentence);
-		if (parsedStrLen > 0)
+		if (getline(&sentence, &size, stdin) == -1)
 		{
-			parsedStr = (char **)malloc((parsedStrLen + 1) * sizeof(char *));
-			if (parsedStr == NULL)
 			{
-				fprintf(stderr, "malloc failed");
-				return (1);
+				freeArr(&sentence);
+				exit(127);
 			}
-			parsedStr[parsedStrLen] = NULL;
-			parseString(sentence, parsedStr);
-			id = fork();
-			if (strcmp(parsedStr[0], "exit") == 0)
-			{
-				freeArr(parsedStr);
-				return (0);
-			}
-			else if (id < 0)
-			{
-				perror("ERR");
-				freeArr(parsedStr);
-				exit(1);
-			}
-			else if (id == 0)
-			{
-				exeCommand(parsedStr);
-			}
-			else
-			{
-				wait(NULL);
-			}
-			freeArr(parsedStr);
 		}
 		else
 		{
-
-			freeArr(&sentence);
-			exit(127);
+			parsedStrLen = numOfWords(sentence);
+			if (parsedStrLen > 0)
+			{
+				parsedStr = (char **)malloc((parsedStrLen + 1) * sizeof(char *));
+				if (parsedStr == NULL)
+				{
+					fprintf(stderr, "malloc failed");
+					return (1);
+				}
+				parsedStr[parsedStrLen] = NULL;
+				parseString(sentence, parsedStr);
+				id = fork();
+				if (strcmp(parsedStr[0], "exit") == 0)
+				{
+					freeArr(parsedStr);
+					return (0);
+				}
+				else if (id < 0)
+				{
+					perror("ERR");
+					freeArr(parsedStr);
+					exit(1);
+				}
+				else if (id == 0)
+				{
+					exeCommand(parsedStr);
+				}
+				else
+				{
+					wait(NULL);
+				}
+				freeArr(parsedStr);
+			}
 		}
 	}
 	exit(0);
