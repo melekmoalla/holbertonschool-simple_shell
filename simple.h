b0ec7b821bb34950f298f69ae0ee4a147b3d2c26
melekmoalla <melekmoalla2468@gmail.com>
2022-12-06 10:15:13 +0100

    modify file simple.h

--- simple.h
+++ simple.h
@@ -2,21 +2,22 @@
 #define INV_TREE_H
 #include <sys/wait.h>
 #include <sys/types.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
 #include <errno.h>
 
 #define SENTENCE_LEN 511
 #define PATH_MAX 512
-
 int main(void);
 int numOfWords(const char sentence[]);
 void parseString(char sentence[], char **parsedStr);
 void freeArr(char **parsedStr);
-int check(char *sentence);
 int exeCommand(char **command);
 
 #endif
